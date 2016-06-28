ae46cc42afbf64e09ff4cf36df189f787a097b8d
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-06-28 14:58:55 -0600

    Socket up and running

--- twitter.js
+++ twitter.js
@@ -9,7 +9,9 @@ let server = app.listen(3000, function () {
   console.log('Listening on port 3000...');
 });
 
-var client = new Twitter({
+let io = require('socket.io').listen(server);
+
+let client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -20,10 +22,18 @@ let hashtags = '#brexit';
 
 client.stream('statuses/filter', {track: hashtags}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    io.emit('newTweet', tweet);
+    // console.log(tweet.text);
   });
 
   stream.on('error', function(error) {
     throw error;
+  });
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
