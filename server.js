e5f85f1e439433f6c4b768e019ce953d596be04d
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-06-29 17:16:13 -0600

    Setup server for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = '3000';
 var app = require('./app');
 var Twitter = require('twitter');
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000...');
 });
 
