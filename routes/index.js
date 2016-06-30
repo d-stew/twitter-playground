186a1c88da895e0cf93a81391dfb919f48a3bb2a
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-06-30 14:54:05 -0600

    Styled main dashboard

--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/watson', function(req, res, next) {
-  var data = req.body.englishData;
-
-  tone_analyzer.tone({ text: data },
-  function(err, tone) {
-    if (err)
-      console.log(err);
-    else
-      res.json(tone, null, 2);
-  });
+  console.log('Testing');
+  // var data = req.body.englishData;
+  //
+  // tone_analyzer.tone({ text: data },
+  // function(err, tone) {
+  //   if (err)
+  //     console.log(err);
+  //   else
+  //     res.json(tone, null, 2);
+  // });
 })
 
 
